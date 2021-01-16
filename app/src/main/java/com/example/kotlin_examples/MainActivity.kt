c646d042d12a60a2f842277a45d4be9a630fc2a1
Rendi Dwi Hermawan <rendidwih2@gmail.com>
2021-01-16 22:54:10 +0700

    theme

--- app/src/main/java/com/example/kotlin_examples/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_examples/MainActivity.kt
@@ -6,38 +6,44 @@ import android.os.Bundle
 //import android.widget.ImageView
 import com.example.kotlin_examples.databinding.TipAppBinding
 import java.text.NumberFormat
-class MainActivity :AppCompatActivity(){
-    private lateinit var binding: TipAppBinding
 
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = TipAppBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.calculateButton.setOnClickListener{calculateTip()}
-    }
-    private fun calculateTip() {
-        val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDoubleOrNull()
-        if (cost == null){
-            binding.tipResult.text = ""
-            return
-        }
-        val tipPercentage = when(binding.tipOptions.checkedRadioButtonId){
-            R.id.option_twenty_percent -> 0.20
-            R.id.option_eighten_percent -> 0.18
-            else -> 0.15
-        }
-        var tip = tipPercentage * cost
-        if (binding.roundUpSwich.isChecked){
-            tip = kotlin.math.ceil(tip)
-        }
-        displayTip(tip)
-    }
-    private fun displayTip(tip: Double){
-        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
-        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+        setContentView(R.layout.theme_page)
     }
 }
+//    private lateinit var binding: TipAppBinding
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        binding = TipAppBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//        binding.calculateButton.setOnClickListener{calculateTip()}
+//    }
+//    private fun calculateTip() {
+//        val stringInTextField = binding.costOfService.text.toString()
+//        val cost = stringInTextField.toDoubleOrNull()
+//        if (cost == null){
+//            binding.tipResult.text = ""
+//            return
+//        }
+//        val tipPercentage = when(binding.tipOptions.checkedRadioButtonId){
+//            R.id.option_twenty_percent -> 0.20
+//            R.id.option_eighten_percent -> 0.18
+//            else -> 0.15
+//        }
+//        var tip = tipPercentage * cost
+//        if (binding.roundUpSwich.isChecked){
+//            tip = kotlin.math.ceil(tip)
+//        }
+//        displayTip(tip)
+//    }
+//    private fun displayTip(tip: Double){
+//        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+//        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+//    }
+//}
 
 //class MainActivity : AppCompatActivity() {
 //    override fun onCreate(savedInstanceState: Bundle?) {
